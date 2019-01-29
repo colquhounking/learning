e773bf8e04361785986730eef5354f552c5b440e
colquhounking <colquhounking@gmail.com>
2019-01-29 22:14:53 +1100

    making the test match the talk - don't ask any more questions

--- pigeonpoem/spec/pigeon_poem_spec.rb
+++ pigeonpoem/spec/pigeon_poem_spec.rb
@@ -7,15 +7,15 @@ describe PigeonPoem do
 		expect(pigeon_poem.convert_dna_bin('ATGC')).to eq '0011'
 	end
 	
-	it 'should convert TCTTGCAGACGCGTAGTTGTAAAATCCTAAGATCCTCCGGTGGATGATACCGGTAGATGTAATTAGGACTA to 01001101011110010010000001100010011011110110010001111001001000000110100' do
-		expect(pigeon_poem.convert_dna_bin('TCTTGCAGACGCGTAGTTGTAAAATCCTAAGATCCTCCGGTGGATGATACCGGTAGATGTAATTAGGACTA')).to eq '01001101011110010010000001100010011011110110010001111001001000000110100'
+	it 'should convert ACGCGTAG to 01111001' do
+		expect(pigeon_poem.convert_dna_bin('ACGCGTAG')).to eq '01111001'
 	end
 
 	it 'should convert 01111001 to y' do
 		expect(pigeon_poem.convert_bin_ascii('01111001')).to eq 'y'
 	end
 
-	it 'should convert TCTTGCAGACGCGTAGTTGTAAAATCCTAAGATCCTCCGGTGGATGATACCGGTAGATGTAATTAGGACTA to see below' do
-		expect(pigeon_poem.convert('TCTTGCAGACGCGTAGTTGTAAAATCCTAAGATCCTCCGGTGGATGATACCGGTAGATGTAATTAGGACTA')).to eq 'My body '
+	it 'should convert TCTTGCAGACGCGTAG to My' do
+		expect(pigeon_poem.convert('TCTTGCAGACGCGTAG')).to eq 'My'
 	end
 end
